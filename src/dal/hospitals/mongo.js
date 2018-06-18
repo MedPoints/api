3bf7a1b196c873f3bff78f59329facec1481cb41
DenisKramarchuk <kramarchuk@protonmail.com>
2018-06-19 00:37:35 +0300

    get all hospitals

--- src/dal/hospitals/mongo.js
+++ src/dal/hospitals/mongo.js
@@ -1,5 +1,6 @@
 const collectionName = 'hospitals';
 
+const ObjectId = require('mongodb').ObjectId;
 
 /**
  * @typedef {Object} OpeningHours -- opening hours model
@@ -17,7 +18,7 @@ const collectionName = 'hospitals';
 
 /**
  * @typedef {Object} Hospital -- hospital model
- * @property {Stirng} _id
+ * @property {String} _id
  * @property {String} name -- hospital name
  * @property {String} chain -- chain of hospital
  * @property {Array<String>} departments -- departments of hospitals
@@ -26,7 +27,7 @@ const collectionName = 'hospitals';
  * @property {String} address -- hospital address
  * @property {Array<String>} photos -- links for photos
  * @property {Array<OpeningHours>} openingHours
- * @property {String} email 
+ * @property {String} email
  * @property {String} phone
  * @property {String} website
  * @property {Array<Doctor>} doctors
@@ -34,53 +35,58 @@ const collectionName = 'hospitals';
 
 /**
  * @param {String} id
- * @returns {Promise<Hospital>} 
+ * @returns {Promise<Hospital>}
  */
 exports.getHospitalById = async function(id){
-    return hospitalQuery.call(this, {_id: id});
+    return hospitalQuery.call(this, {_id: ObjectId(id)});
 };
 
 /**
  * @param {String} name
  * @return {Promise<Hospital>}
  */
-exports.getHospitalByByName = async function(name){
+exports.getHospitalByName = async function(name){
     return hospitalQuery.call(this, {name});
 };
 
+exports.getAllHospitals = async function() {
+	const collection = this.mongo.collection(collectionName);
+	return collection.find(filter).toArray();
+};
+
 /**
- * @param {Hospital} hospital 
+ * @param {Hospital} hospital
  */
 exports.saveHospital = async function(hospital){
     const collection = this.mongo.collection(collectionName);
-    await collection.save(hospital);
+    await collection.insert(hospital);
 };
 
 /**
- * 
- * @param {String} id 
- * @param {Hospital} hospital 
+ *
+ * @param {String} id
+ * @param {Hospital} hospital
  */
 exports.updateHospital = async function(id, hospital){
     const collection = this.mongo.collection(collectionName);
-    await collection.update({_id: id}, hospital);
+    await collection.update({_id: ObjectId(id)}, hospital);
 };
 
 
 /**
- * @param {String} id 
+ * @param {String} id
  */
 exports.deleteHospital = async function(id){
     const collection = this.mongo.collection(collectionName);
-    await collection.remove({_id, id});
+    await collection.remove({_id: ObjectId(id)});
 };
 
 /**
  * @param {Object} filter
- * @returns {Promise<Hospital>} 
+ * @returns {Promise<Hospital>}
  */
 async function hospitalQuery(filter){
     const collection = this.mongo.collection(collectionName);
     const hospitals = await collection.find(filter).limit(1).toArray();
     return hospitals[0];
-} 
+}
