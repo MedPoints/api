da650085f09fca77bace00909ecceec1d4b86ee1
DenisKramarchuk <kramarchuk@protonmail.com>
2018-08-05 21:21:34 +0300

    fix model

--- src/models/pharmacies.js
+++ src/models/pharmacies.js
@@ -2,12 +2,18 @@ const ObjectId = require("mongodb").ObjectId;
 const LocationModel = require('./location');
 
 class BasePharmaciesModel {
-	constructor({name, description, location, workTime, work_time, drugs}){
+	constructor({name, description, network, location, workTime, work_time, drugs, photos, country, city, address, short_descr}){
 		this.name = name;
+		this.network = network;
 		this.description = description;
 		this.location = new LocationModel(location);
+		this.country = country;
+		this.city = city;
+		this.address = address;
 		this.work_time = workTime || work_time;
+		this.photos = photos || [];
 		this.drugs = drugs || [];
+		this.short_descr = short_descr || [];
 	}
 }
 
