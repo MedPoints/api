fc4aa28b6f9dd31adc786eaabb7fb86ba140da7e
DenisKramarchuk <kramarchuk@protonmail.com>
2018-09-16 19:26:28 +0300

    fix link

--- src/notifications/index.js
+++ src/notifications/index.js
@@ -13,7 +13,7 @@ exports.sendConfirmMessage = async function({token, email, firstName, lastName})
 	const file = await Promise.promisify(fs.readFile, {context: fs})(path.join(__dirname, 'templates', 'register.html'));
 	const template = handlebars.compile(file.toString());
 	const fullname = `${firstName} ${lastName}`;
-	const link = `http://46.101.121.69:8080/api/auth/confirm?token=${token}`;
+	const link = `http://46.101.121.69:8080/api/users/confirm?token=${token}`;
 	const html = template({
 		fullname: fullname,
 		link: link,
