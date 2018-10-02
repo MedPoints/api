60098f76e464cecb67eab8297df1dfbb2ddb101d
DenisKramarchuk <kramarchuk@protonmail.com>
2018-10-02 22:40:30 +0300

    fix connections

--- src/dal/mongodal.js
+++ src/dal/mongodal.js
@@ -1,4 +1,6 @@
-const MongoClient = require('mongodb').MongoClient;
+'use strict';
+
+const {MongoClient} = require('mongodb');
 
 
 class MongoDal {
@@ -9,18 +11,28 @@ class MongoDal {
 		this._config = {};
 		this._initConfig(config);
 		this._client = null;
+		this._connectionCount = 0;
 	}
 	
 	/**
 	 * @returns {Promise<Db>}
 	 */
 	async createConnection(){
-		this._client = await MongoClient.connect(this._config.url, {});
+		if(!this._client){
+			this._client = await MongoClient.connect(this._config.url, {});
+		}
+		this._connectionCount++;
 		return this._client.db(this._config.dbName);
 	}
 
 	close(){
-		this._client.close();
+		this._connectionCount--;
+		if(this._connectionCount === 0){
+			this._client.close();
+			this._client = null;
+		} else if (this._connectionCount < 0){
+			throw new Error('CONNECTION_ALREADY_CLOSED');
+		}
 	}
 
 	/**
