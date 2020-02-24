3eb00ec45aa8a6381fa0af2461e3f7378a290d4a
k0t0v <maxim.kotov@tutanota.com>
2020-02-24 06:27:52 +0300

    uploadCreate fix

--- src/models/uploads.js
+++ src/models/uploads.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const ObjectId = require("mongodb").ObjectId;
+
 class Upload {
 	constructor({publicKey, transactionId, fullname, filename, timestamp, extension}){
 		this.publicKey = publicKey;
